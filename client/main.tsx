8f5bb8dc25a0585444b1303e825808a670dc949c
capital terefe <capital.terefe@gmail.com>
2026-01-05 21:13:03 -0500

    Initialize React application in main.tsx

---
+++ client/main.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+
+const root = document.getElementById("root");
+
+if (root) {
+  const reactRoot = createRoot(root);
+  reactRoot.render(<App />);
+}
